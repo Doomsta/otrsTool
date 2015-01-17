1db1d945607f630e4bfc53b315fe997f4acb40e6
arandel <andre.randel@sectornord.de>
2015-01-17 19:13:07 +0100

    add twig; implement repo:buildIndex

--- src/OtrsTool/Command/Repo/BuildIndexCommand.php
+++ src/OtrsTool/Command/Repo/BuildIndexCommand.php
@@ -6,7 +6,11 @@ namespace OtrsTool\Command\Repo;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Twig_Loader_Filesystem;
 
 class BuildIndexCommand extends Command
 {
@@ -19,11 +23,66 @@ class BuildIndexCommand extends Command
             'dir where the index will be build',
             './'
         );
-        
+        $this->addOption('Print', 'P', InputOption::VALUE_NONE, 'prints the sopm');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        throw new \Exception('#TODO');
+        $dir = $input->getArgument('dir');
+        $print = (bool) $input->getOption('Print');
+        $opms = [];
+        foreach((new Finder())->in($dir)->name('*.opm') as $file) {
+            /** @var SplFileInfo $file */
+            try {
+                $opm = new \SimpleXMLElement($file->getContents());
+                $opms[] = $this->getPackageData($opm, $file->getRelativePathName());
+                
+            } catch (\Exception $e) {
+                $output->writeln('skipped!!');
+            }
+
+        }
+        $index = (new \Twig_Environment(new Twig_Loader_Filesystem([__DIR__.'/../../Templates'])))
+            ->render('otrs.xml.twig', ['opms' =>$opms]);
+        if($print) {
+            $output->writeln($index);
+        } else {
+            if(file_put_contents($dir.'/otrs.xml', $index)) {
+                $output->writeln('wrote new otrs.xml with '.count($opms).' entries');
+                return 0;
+            } else {
+                $output->writeln('failed!');
+                return 1;
+            }
+        }
+        return 1;
     }
+
+    /**
+     * @TODO return a opm obj
+     * @param \SimpleXMLElement $opm
+     * @param string $path
+     * @return array
+     */
+    private function getPackageData(\SimpleXMLElement $opm, $path)
+    {
+        $data = [
+            'name' => (string) $opm->Name,
+            'version' => (string) $opm->Version,
+            'url' => (string) $opm->URL,
+            'vendor' => (string) $opm->Vendor,
+            'license' => (string )$opm->License,
+            'framework' =>(string) $opm->Framework,
+            'path' => '/'.$path
+        ];
+        foreach($opm->Description as $desc) {
+
+            $data['descs'][] = [
+                'lang' => (string)$desc['Lang'],
+                'str' => (string)$desc,
+            ];
+        }
+        return $data;
+    }
+
 }
